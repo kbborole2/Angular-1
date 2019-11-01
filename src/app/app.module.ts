f68e8db180b8d8d957b9b3ab23bd2da1abf9bdb2
KB <kbborole2@gmail.com>
2019-11-01 09:03:36 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './notfound.component';;
+import { routing } from './app.routing';
+import { AuthGuard } from './login/auth-guard.service';
+import { LoginService } from './login/login.service';
+import { PreventUnsavedChangesGuard } from './login/prevent-unsaved-changes-guard.service';
+import { GitHubModule } from './github/github.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    GitHubModule,
+    LoginModule,
+    routing
   ],
-  providers: [],
+  providers: [AuthGuard,LoginService, PreventUnsavedChangesGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/github/githubuser.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'user',
+    template: `
+        <h1>User Login: {{ login }}</h1>
+        <h2>User Score: {{ score }}</h2>   
+    `
+})
+export class GitHubUserComponent  {
+    login;
+    score;
+    subscription;
+
+    constructor(private _route: ActivatedRoute){
+    }
+
+    ngOnInit(){
+        this.subscription = this._route.params.subscribe(params => {
+            this.login = params["login"];
+            this.score = params["score"];
+        })
+    }
+
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
+    }
+
+
+    
+}
